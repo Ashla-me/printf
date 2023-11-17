0fe551ed9b8250148babd0c1d8c98abf5c1a7968
Ashlame <mercyashlamesunday@gmail.com>
2023-11-17 09:33:24 -0800

    print_character

---
+++ print_character.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_char - funtion thats prints a character
+ * @c: list of arguments
+ * Return: returns the number of characters
+ */
+
+int print_char(va_list c)
+{
+        char s;
+
+        s = va_arg(c, int);
+        _print(s);
+        return (1);
+}
