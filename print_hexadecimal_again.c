73e96e0c0e3a344c108e22bef72239f188cb3764
Ashlame <mercyashlamesunday@gmail.com>
2023-11-17 19:01:26 -0800

    hexacidecimal_again

---
+++ print_hexadecimal_again.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * print_hex_again - prints an hexgecimal number.
+ * @num: number to print.
+ * Return: counter.
+ */
+int print_hex_again(unsigned int num)
+{
+int k;
+int *array;
+int counter = 0;
+unsigned int tem = num;
+
+while (num / 16 != 0)
+{
+num /= 16;
+counter++;
+}
+counter++;
+array = malloc(counter *sizeof(int));
+
+for (k = 0; k < counter; k++)
+{
+array[k] = tem % 16;
+tem /= 16;
+}
+for (k = counter - 1; k >= 0; k--)
+{
+if (array[k] > 9)
+array[k] = array[k] + 7;
+_putchar(array[k] + '0');
+}
+free(array);
+return (counter);
+}
